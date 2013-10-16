0556195a1276803e52b71f0f88d1da34f244e4a8
ohohyeah <ohohyeah0515@hotmail.com>
2013-10-16 20:00:04 +0800

    rewrite strcmp, because it has bug in the previous strcmp

--- string-util.c
+++ string-util.c
@@ -112,20 +112,20 @@ size_t strlen(const char *string)
 }
 
 
+
 int strcmp(const char *a, const char *b)
 {
-        asm(
-        "strcmp_lop:                \n"
-        "   ldrb    r2, [r0],#1     \n"
-        "   ldrb    r3, [r1],#1     \n"
-        "   cmp     r2, #1          \n"
-        "   it      hi              \n"
-        "   cmphi   r2, r3          \n"
-        "   beq     strcmp_lop      \n"
-                "        sub     r0, r2, r3          \n"
-        "   bx      lr              \n"
-                :::
-        );
+    int i = 0;
+
+    while(a[i]) 	
+    {
+        if (a[i] != b[i]) 
+	{
+            return a[i] - b[i];
+        }
+        i++;
+    }
+    return 0;
 }
 
 void myprintf(const char *fmt, ...)
